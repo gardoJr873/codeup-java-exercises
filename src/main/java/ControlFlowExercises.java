6031050404d91fd3c4f26df8c4eda4ceb5d729ff
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-02-07 15:56:44 -0600

    Java control flow exercise Do While loop to count backwards by 5's from 100 to -10.

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int count = 0;
+        int count = 100;
         do{
             System.out.println("Count is equal to " + count);
-            count+=2;
-        } while (count < 100);
+            count-=5;
+        } while (count > -15);
     }
 }
