08d6e9d2f5435cffb0255b6e55cb658e808d8b0b
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-02-07 15:41:01 -0600

    Java Loop basics exercises While Loop

---
+++ src/main/java/ControlFlowExercises.java
@@ -0,0 +1,9 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int count = 5;
+        while (count < 15) {
+            System.out.println("The current value of i is " + count);
+            count++;
+        }
+    }
+}
