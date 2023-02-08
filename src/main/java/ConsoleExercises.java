973d1e9c4fed717f9ff4afb75dbfdd6c4185bb11
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-02-08 10:39:18 -0600

    Java control flow displays the for loop

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        System.out.format("The value of pi is approximately" + " %.2f%n", pi);
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately" + " %.2f%n", pi);
 //
 
         Scanner scanner = new Scanner(System.in);
