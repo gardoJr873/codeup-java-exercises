5d2908dfd3292b0eba1ed06eaf801bb0b3668df8
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-02-08 09:37:06 -0600

    Java control flow displays the number squared

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int count = 100;
+        int count = 2;
         do{
-            System.out.println("Count is equal to " + count);
-            count-=5;
-        } while (count > -15);
+            System.out.println("Count squared is equal to " + count);
+            count *= 2 ;
+        } while (count < 1000000);
     }
 }
