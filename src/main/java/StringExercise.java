c6164976f86d50306165c3cc73c05a956a6b4d4e
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-02-09 13:35:18 -0600

    Java String exercise submission

---
+++ src/main/java/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String myStrPink = "We don't need no education";
+        System.out.println(myStrPink);
+
+        String myStringFloyd = "We don't need no thought control";
+        System.out.println(myStringFloyd);
+
+        String myStrTeen = "Check \"this\" out!, s inside of "+'s'+"!";
+        System.out.println(myStrTeen);
+
+        String myStrGates = "In Windows, the main drive is usually C:\\";
+        System.out.println(myStrGates);
+
+        String myStrJester = "I can do backslashes \\, double backslashes \\\\, " +
+                "and the amazing triple backslash \\\\\\";
+        System.out.println(myStrJester);
+    }
+}
