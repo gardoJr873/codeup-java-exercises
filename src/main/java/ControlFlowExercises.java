c0b095f9f7eba8ead0750c7e9e5626010939d649
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-02-14 14:49:55 -0600

    Java control flow submission

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -3,25 +3,35 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
+        Scanner gradeInput = new Scanner(System.in);
         Scanner choice = new Scanner(System.in);
+
         do {
-            System.out.println("What number would you like to go up to?");
-            double numChoice = Integer.parseInt(choice.nextLine());
+            System.out.println("Enter numeric grade");
+            double grade = gradeInput.nextDouble();
 
-            System.out.printf("Here is your table!%n number | squared | cubed %n --------- | ---------- | --------%n");
-
-            for (int i = 1; i <= numChoice; i++) {
-                System.out.println(i + " \t\t\t   |  " + Math.pow(i, 2) + "\t\t|\t  " + (Math.pow(i, 3)));
+            if(grade >= 88) {
+                System.out.println("You scored an A");
+            } else if (grade >= 80) {
+                System.out.println("You scored a B");
+            } else if (grade >= 67) {
+                System.out.println("You scored a C");
+            } else if (grade >= 60) {
+                System.out.println("You scored a D");
+            } else if (grade >= 59){
+                System.out.println("You scored an F");
+            } else if ( grade >= 0) {
+                System.out.println("You scored an F");
             }
-                System.out.println("Would you like to go again ");
-                String userChoice = String.valueOf(choice.nextLine());
 
-            if(userChoice.equalsIgnoreCase("no")){
+            System.out.println("continue");
+            String userChoice = choice.nextLine();
+
+            if(userChoice.equalsIgnoreCase("no")) {
                 System.out.println("Thank You");
                 break;
             }
         } while (true);
-        choice.close();
     }
-
 }
